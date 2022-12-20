b985837c6bed815efdfb21cf600471bd74745583
kumarmahindra <121093989+kumarmahindra@users.noreply.github.com>
2022-12-20 15:44:00 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,11 @@
+class Newprogram
+{ 
+   public static void main(String args[])
+   { 
+     Scanner s= new Scanner(System.in)
+     System.out.println("enter two numbers");
+     int a=s.nextInt();
+     int b=s.nextInt();
+     System.out.println("sum of two numbers is"+(a+b));
+   } 
+}
